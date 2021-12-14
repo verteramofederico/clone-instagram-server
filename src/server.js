f9139e304d783d8c5a2867caae647b6ec0db62e8
verteramofederico <verteramo.federico@gmail.com>
2021-12-14 14:17:13 -0300

    router added

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
+const routes = require('./routes')
+
 const app = express();
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('todo ok')
-})
+app.use(routes)
 
 app.listen(process.env.Port || 3333)

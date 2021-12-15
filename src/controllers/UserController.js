870e9e6a3db284719ff870551aa9badcda81f502
verteramofederico <verteramo.federico@gmail.com>
2021-12-15 14:14:27 -0300

    db connected

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,8 +1,15 @@
-
+const sequelize = require("sequelize");
+const db = require('../database/models');
 
 module.exports = {
     async store(req, res) {
         const {name, email, username, password} = req.body
-        res.json({name})
+        const user = await db.User.create({
+            name, 
+            email, 
+            username, 
+            password
+        });
+        res.json({user})
     }
 }

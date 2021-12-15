736b905752c14bfc23e86a63a7c644d12722fd43
verteramofederico <verteramo.federico@gmail.com>
2021-12-15 14:41:49 -0300

    express validation added. working already on store user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,15 +1,36 @@
-const sequelize = require("sequelize");
+const Sequelize = require("sequelize");
+const { validationResult } = require("express-validator");
 const db = require('../database/models');
+
+let User = db.User // model
 
 module.exports = {
     async store(req, res) {
         const {name, email, username, password} = req.body
-        const user = await db.User.create({
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        let user = await User.findOne({
+            where: { [Sequelize.Op.or]: [{ email }, { username }] }
+        })
+
+        if (user) {
+            if (user.email === email)
+                return res.status(400).json({ message: "Este email ya esta en uso" });
+            if (user.username === username)
+                return res.status(400).json({ message: "Este usuario ya esta en uso" });
+        }
+        
+        user = await User.create({
             name, 
             email, 
             username, 
             password
-        });
+        })
+
         res.json({user})
     }
 }
